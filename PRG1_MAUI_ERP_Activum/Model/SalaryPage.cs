bebd57a254480ef67db992bb4875c3ec7ac2e7ea
Kroaxys <mellar08sw@gmail.com>
2026-03-25 12:56:48 +0100

    Added viewing current user in SalaryPage.xaml.cs

---
+++ PRG1_MAUI_ERP_Activum/Model/SalaryPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRG1_MAUI_ERP_Activum.Model
+{
+    public class SalaryPageClass
+    {
+        public static int CurrentUser = 0;
+    }
+}
--- PRG1_MAUI_ERP_Activum/View/SalaryPage.xaml.cs
+++ PRG1_MAUI_ERP_Activum/View/SalaryPage.xaml.cs
@@ -1,9 +1,14 @@
 namespace PRG1_MAUI_ERP_Activum.View;
+using PRG1_MAUI_ERP_Activum.Model;
+using System.Diagnostics;
 
 public partial class SalaryPage : ContentPage
 {
 	public SalaryPage()
 	{
 		InitializeComponent();
-	}
+		int CurrentUser = SalaryPageClass.CurrentUser;
+		Debug.WriteLine(CurrentUser);
+
+    }
 }
