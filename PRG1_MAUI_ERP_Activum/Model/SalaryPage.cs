b6703a1dd1e2a6ff12559d16dff339b1825bff78
Kroaxys <mellar08sw@gmail.com>
2026-03-27 13:23:52 +0100

    Added class and reading JSON file

--- PRG1_MAUI_ERP_Activum/Model/SalaryPage.cs
+++ PRG1_MAUI_ERP_Activum/Model/SalaryPage.cs
@@ -8,4 +8,15 @@ namespace PRG1_MAUI_ERP_Activum.Model
     {
         public static int CurrentUser = 0;
     }
+    
+    public class Employee
+    {
+        public int BaseSalary { get; set; }
+        public int CommissionEarnedThisMonth { get; set; }
+        public double CommissionRate { get; set; }
+        public required string Email { get; set; }
+        public required string Name { get; set; }
+        public required string Password { get; set; }
+        public int TotalEarnedThisYear { get; set; }
+    }
 }
--- PRG1_MAUI_ERP_Activum/View/SalaryPage.xaml.cs
+++ PRG1_MAUI_ERP_Activum/View/SalaryPage.xaml.cs
@@ -1,14 +1,29 @@
 namespace PRG1_MAUI_ERP_Activum.View;
 using PRG1_MAUI_ERP_Activum.Model;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Text.Json;
 
 public partial class SalaryPage : ContentPage
 {
+    string path;
 	public SalaryPage()
 	{
 		InitializeComponent();
 		int CurrentUser = SalaryPageClass.CurrentUser;
 		Debug.WriteLine(CurrentUser);
+        string basePath = Directory.GetCurrentDirectory();
+        string shortendPath = Directory.GetParent(basePath).Parent.Parent.Parent.FullName;
+        path = Path.Combine(shortendPath, "Storage", "UserData.json");
+        Debug.WriteLine(path);
+        Load();
+    }
 
+	private void Load()
+	{
+        string json = File.ReadAllText(path);
+        //ObservableCollection<Employee> employees = JsonSerializer.Deserialize<ObservableCollection<Employee>>(json);
+        //Debug.WriteLine(employees[0]);
+        Debug.WriteLine(json);
     }
 }
